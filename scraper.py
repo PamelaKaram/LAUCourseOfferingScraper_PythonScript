177e3973e2cc90dd74fe7106c1ded08286eced2f
PamelaKaram <pamela.karam01@lau.edu>
2023-05-08 18:11:50 +0300

    Enter login credentials and submit

--- scraper.py
+++ scraper.py
@@ -1,6 +1,8 @@
 import sys 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+
 
 
 if len(sys.argv) < 3:
@@ -20,3 +22,9 @@ chr_options.binary_location = 'C:/Program Files/Google/Chrome/Application/chrome
 # Go to website
 driver = webdriver.Chrome(executable_path="../chromedriver.exe", chrome_options=chr_options)
 driver.get("https://myportal.lau.edu.lb/")
+#enter username and password and click enter
+driver.find_element("name", "username").send_keys(username)  
+driver.find_element("name", "password").send_keys(password)
+driver.find_element("xpath", "//input[@type='submit' and @value='Log In']").send_keys(Keys.ENTER)
+
+
