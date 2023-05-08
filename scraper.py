7d5f5aba70725cf6706e8717272fd29d40c31f25
PamelaKaram <pamela.karam01@lau.edu>
2023-05-08 17:25:07 +0300

    took username and pass as arguments

---
+++ scraper.py
@@ -0,0 +1,8 @@
+import sys 
+if len(sys.argv) < 3:
+    print("Please provide both username and password as command-line arguments")
+    sys.exit()
+
+username = sys.argv[1]
+password = sys.argv[2]
+
