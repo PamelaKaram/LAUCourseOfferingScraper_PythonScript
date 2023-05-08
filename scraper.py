608ad02ca00400be58615af539431a39294367a0
PamelaKaram <pamela.karam01@lau.edu>
2023-05-08 17:59:24 +0300

    Set up Chrome driver and launch lau website

--- scraper.py
+++ scraper.py
@@ -1,4 +1,8 @@
 import sys 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+
 if len(sys.argv) < 3:
     print("Please provide both username and password as command-line arguments")
     sys.exit()
@@ -6,3 +10,13 @@ if len(sys.argv) < 3:
 username = sys.argv[1]
 password = sys.argv[2]
 
+# Set up service for Chrome driver
+service = Service(executable_path="../chromedriver.exe")
+
+# Set binary location for Chrome
+chr_options = webdriver.ChromeOptions()
+chr_options.binary_location = 'C:/Program Files/Google/Chrome/Application/chrome.exe'
+
+# Go to website
+driver = webdriver.Chrome(executable_path="../chromedriver.exe", chrome_options=chr_options)
+driver.get("https://myportal.lau.edu.lb/")
